b5f50710dd3799aa219110bdf00c00c18fa0e059
Denis Bystruev <dbystruev@me.com>
2019-04-06 15:16:58 +0300

    Initial Commit

---
+++ Universal Reference/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Universal Reference
+//
+//  Created by Denis Bystruev on 06/04/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
